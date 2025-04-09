93fd33ccd16bf175e4bdf06d4051eeed264da362
Shubham Mathur <shubham.mathur@tide.co>
2025-04-09 16:21:06 +0530

    updated to localhost

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2252912739"
+    serviceWorkerVersion: "1902934612"
   }
 });
